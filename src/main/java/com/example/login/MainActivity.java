3192be0714a407f97d470a7e234ed54fd8507ea1
Ganeshprasad Keni <ganeshkeni24@gmail.com>
2020-11-07 14:31:07 +0530

    Initial Commit

--- src/main/java/com/example/login/MainActivity.java
+++ src/main/java/com/example/login/MainActivity.java
@@ -7,13 +7,30 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth pauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pauth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser cuser = pauth.getCurrentUser();
+        if(cuser != null)
+        {
+
+        }
+        else
+        {
+            startActivity(new Intent(getApplicationContext(), login_page.class));
+            finish();
+        }
     }
 
     public void logout_click(View view) {
